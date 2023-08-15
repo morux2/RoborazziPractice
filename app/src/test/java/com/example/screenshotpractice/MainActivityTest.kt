94a09cb6883c4864abeee34977f71607b544fa15
morux2 <k-morimoto@keio.jp>
2023-08-16 00:32:54 +0900

    add several device config

--- app/src/test/java/com/example/screenshotpractice/MainActivityTest.kt
+++ app/src/test/java/com/example/screenshotpractice/MainActivityTest.kt
@@ -3,10 +3,12 @@ package com.example.screenshotpractice
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onRoot
 import androidx.test.runner.AndroidJUnit4
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
 import com.github.takahirom.roborazzi.captureRoboImage
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 import org.robolectric.annotation.GraphicsMode
 
 @RunWith(AndroidJUnit4::class)
@@ -17,7 +19,15 @@ class MainActivityTest {
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
-    fun captureMainActivity() {
+    @Config(qualifiers = RobolectricDeviceQualifiers.Pixel5)
+    fun captureMainActivityPixel5() = captureMainActivity()
+
+    @Test
+    // https://github.com/takahirom/roborazzi/issues/47
+    @Config(qualifiers = RobolectricDeviceQualifiers.MediumTablet)
+    fun captureMainActivityMediumTablet() = captureMainActivity()
+
+    private fun captureMainActivity() {
         composeTestRule
             .onRoot()
             .captureRoboImage()
