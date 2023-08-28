dfa66fb5666022784d3847e56bb0a4814edfaa31
morux2 <k-morimoto@keio.jp>
2023-08-28 20:38:42 +0900

    introduce Preview screenshot testing with Showkase and Roborazzi

---
+++ app/src/main/java/com/example/screenshotpractice/ShowkaseScreenshotModule.kt
@@ -0,0 +1,7 @@
+package com.example.screenshotpractice
+
+import com.airbnb.android.showkase.annotation.ShowkaseRoot
+import com.airbnb.android.showkase.annotation.ShowkaseRootModule
+
+@ShowkaseRoot
+class ShowkaseScreenshotModule : ShowkaseRootModule
