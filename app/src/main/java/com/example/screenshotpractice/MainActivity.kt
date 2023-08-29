0604bfcb21e3c676af69afc1debb353cd692b8a3
morux2 <k-morimoto@keio.jp>
2023-08-29 14:39:51 +0900

    remove unused import

--- app/src/main/java/com/example/screenshotpractice/MainActivity.kt
+++ app/src/main/java/com/example/screenshotpractice/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.screenshotpractice
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,7 +29,7 @@ fun MainScreen() {
         Text(
             text = "Hello Android!\nHello Android!\nHello Android!",
             color = Color.Gray
-            )
+        )
     }
 }
 
