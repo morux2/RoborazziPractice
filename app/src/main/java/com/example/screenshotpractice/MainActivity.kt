7e8987eec07620a691412178cd7d97a05ebb7da5
morux2 <k-morimoto@keio.jp>
2023-08-29 15:34:49 +0900

    detect corner radius change

--- app/src/main/java/com/example/screenshotpractice/MainActivity.kt
+++ app/src/main/java/com/example/screenshotpractice/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,11 +26,12 @@ class MainActivity : ComponentActivity() {
 fun MainScreen() {
     Surface(
         modifier = Modifier.fillMaxSize(),
-        color = Color(0xFFFEFEFF)
+        color = Color.Gray,
+        shape = RoundedCornerShape(4.dp)
     ) {
         Text(
             text = "Hello Android!\nHello Android!\nHello Android!",
-            color = Color.Gray
+            color = Color.Black
         )
     }
 }
