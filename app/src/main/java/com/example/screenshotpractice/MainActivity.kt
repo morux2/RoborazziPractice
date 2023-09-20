56f753ba4f26c9c8da21521a11c58f6f6f76390c
morux2 <k-morimoto@keio.jp>
2023-09-20 10:36:19 +0900

    recordRoborazziDebug never ends if lottie animation iterates forever

--- app/src/main/java/com/example/screenshotpractice/MainActivity.kt
+++ app/src/main/java/com/example/screenshotpractice/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.screenshotpractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +29,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    Surface(
+    Column(
         modifier = Modifier.fillMaxSize(),
-        color = Color.White
     ) {
         Text(text = "Hello Android!\nHello Android!\nHello Android!")
+        val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.heart))
+        LottieAnimation(
+            composition = composition,
+            iterations = LottieConstants.IterateForever
+        )
     }
 }
 
