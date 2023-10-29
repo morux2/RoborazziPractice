28a583a0d91e668ce2b61678c4f68fed398b823d
morux2 <k-morimoto@keio.jp>
2023-10-29 13:39:08 +0900

    add captureMultiDevice

--- app/src/test/java/com/example/screenshotpractice/LongContentActivityTest.kt
+++ app/src/test/java/com/example/screenshotpractice/LongContentActivityTest.kt
@@ -5,12 +5,9 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performScrollToNode
 import androidx.test.runner.AndroidJUnit4
-import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
-import com.github.takahirom.roborazzi.captureRoboImage
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
 import org.robolectric.annotation.GraphicsMode
 
 @RunWith(AndroidJUnit4::class)
@@ -20,19 +17,16 @@ class LongContentActivityTest {
     val composeTestRule = createAndroidComposeRule<LongContentActivity>()
 
     @Test
-    @Config(qualifiers = RobolectricDeviceQualifiers.Pixel5)
-    fun captureMainActivityPixel5() = captureLongContentActivity()
+    fun captureLongContentActivity() {
+        composeTestRule.captureMultiDevice(
+            screenshotName = "LongContentActivity",
+            body = { LongContentScreen() },
+            perform = {
+                composeTestRule
+                    .onNodeWithTag("lazyColumn")
+                    .performScrollToNode(hasText("Hello 30"))
 
-    @Test
-    @Config(qualifiers = RobolectricDeviceQualifiers.MediumTablet)
-    fun captureMainActivityMediumTablet() = captureLongContentActivity()
-
-    private fun captureLongContentActivity() {
-        composeTestRule
-            .onNodeWithTag("lazyColumn")
-            .performScrollToNode(hasText("Hello 30"))
-            .captureRoboImage(
-                roborazziOptions = DefaultRoborazziOptions,
-            )
+            }
+        )
     }
 }
