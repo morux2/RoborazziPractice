b0521eec240d44c07af23565a39db684c358e384
morux2 <k-morimoto@keio.jp>
2023-11-04 22:22:14 +0900

    lottie infinite anim

--- app/src/main/java/com/example/screenshotpractice/MainActivity.kt
+++ app/src/main/java/com/example/screenshotpractice/MainActivity.kt
@@ -9,8 +9,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +25,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    Surface(
+    Column(
         modifier = Modifier.fillMaxSize(),
-        color = Color.White
     ) {
         Text(text = "Hello Android!\nHello Android!\nHello Android!")
+        val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.heart))
+        LottieAnimation(
+            composition = composition,
+            iterations = LottieConstants.IterateForever
+        )
     }
 }
 
